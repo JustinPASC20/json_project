f768e4587b88310e65a71911a0b5e027e77e474f
JustinPASC20 <justinperez091000@gmail.com>
2021-08-06 11:50:33 -0400

    Color Scheme

---
+++ script.js
@@ -0,0 +1,56 @@
+
+console.dir(randUser.results[0])
+
+for (let i = 0; i<randUser.results.length; i++){
+let profile = document.querySelector("#profile");
+
+let newDivNames = document.createElement("div");
+
+//Big container
+
+let newDiv = document.createElement("div");
+let textNames = document.createElement("p")
+let textEmails = document.createElement("p")
+
+let imageDiv = document.createElement("div");
+let newImage = document.createElement("img");
+
+
+let dummyDiv = document.createElement("div");
+let dummyName = document.createElement("p");
+let dummyEmail =document.createElement("p");
+
+//Add content
+textNames.innerHTML = "Name: " + randUser.results[i].name.first + " " + randUser.results[i].name.last;
+textEmails.innerHTML = "Email: " + randUser.results[i].email;
+dummyName.innerHTML = "City: " + randUser.results[i].location.state;
+dummyEmail.innerHTML = "Timezone: " + randUser.results[i].location.timezone.offset + ";" + randUser.results[i].location.timezone.description;
+
+newImage.src = randUser.results[i].picture.large;
+
+//Add elements to page
+newDivNames.appendChild(textNames);
+newDivNames.appendChild(textEmails);
+
+//The elements on the right
+dummyDiv.appendChild(dummyName);
+dummyDiv.appendChild(dummyEmail);
+
+imageDiv.style.display="flex";
+imageDiv.style.flexDirection = "row";
+imageDiv.style.justifyContent = "center";
+imageDiv.appendChild(newImage);
+
+newDiv.style.display = "flex";
+newDiv.style.justifyContent = "space-between";
+
+newDiv.appendChild(newDivNames);
+newDiv.appendChild(imageDiv);
+newDiv.appendChild(dummyDiv);
+
+profile.appendChild(newDiv)
+
+}
+
+
+
