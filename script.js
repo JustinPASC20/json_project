e6e82ad272a1044c717e0c3db723353e4a0cde09
JustinPASC20 <justinperez091000@gmail.com>
2021-08-06 12:18:52 -0400

    Button Addition

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let newDivNames = document.createElement("div");
 let newDiv = document.createElement("div");
 let textNames = document.createElement("p")
 let textEmails = document.createElement("p")
+let button = document.createElement("button")
+
 
 let imageDiv = document.createElement("div");
 let newImage = document.createElement("img");
@@ -23,8 +25,13 @@ let dummyEmail =document.createElement("p");
 //Add content
 textNames.innerHTML = "Name: " + randUser.results[i].name.first + " " + randUser.results[i].name.last + ", " + randUser.results[i].dob.age;
 textEmails.innerHTML = "Email: " + randUser.results[i].email;
+button.innerHTML = "Connect"
 dummyName.innerHTML = "City: " + randUser.results[i].location.state;
 dummyEmail.innerHTML = "Timezone: " + randUser.results[i].location.timezone.offset + ";" + randUser.results[i].location.timezone.description;
+
+button.onclick = function() {
+    alert("Connecting...")
+}
 
 newImage.src = randUser.results[i].picture.large;
 
@@ -43,7 +50,9 @@ imageDiv.appendChild(newImage);
 
 newDiv.style.display = "flex";
 newDiv.style.justifyContent = "space-between";
+button.style.display = "block"
 
+newDiv.appendChild(button)
 newDiv.appendChild(newDivNames);
 newDiv.appendChild(imageDiv);
 newDiv.appendChild(dummyDiv);
@@ -51,6 +60,7 @@ newDiv.appendChild(dummyDiv);
 profile.appendChild(newDiv)
 
 }
+
 
 
 
